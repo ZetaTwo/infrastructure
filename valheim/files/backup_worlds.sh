ad1f56d3ec0bae7660f3dfc7dbe91de72170f9c9
Calle Svensson <calle.svensson@zeta-two.com>
2022-12-28 23:13:23 +0100

    Update Valheim backup script

--- valheim/files/backup_worlds.sh
+++ valheim/files/backup_worlds.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ~/.config/unity3d/IronGate/Valheim
-tar zcf ~/worlds_backups/worlds_$(date +%Y-%m-%d_%H-%M).tar.gz worlds
+tar zcf ~/worlds_backups/worlds_$(date +%Y-%m-%d_%H-%M).tar.gz worlds_local
